2b9d67383cf407f7b5c46cc9e5d5c5e5a80b2078
tmsfreitas <thiago.msfreitas@hotmail.com>
2023-03-03 23:09:14 -0300

    Adição de classes de domínio Dev e Bootcamp

---
+++ src/Dev.java
@@ -0,0 +1,53 @@
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Dev {
+    private String nome;
+    private Set<Conteudo> conteudosIncritos = new LinkedHashSet<>();
+    private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
+
+    public void inscreverBootcamp(Bootcamp bootcamp){}
+
+    public void progredir(){}
+
+    public void calcularXp(){}
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteudo> getConteudosIncritos() {
+        return conteudosIncritos;
+    }
+
+    public void setConteudosIncritos(Set<Conteudo> conteudosIncritos) {
+        this.conteudosIncritos = conteudosIncritos;
+    }
+
+    public Set<Conteudo> getConteudosConcluidos() {
+        return conteudosConcluidos;
+    }
+
+    public void setConteudosConcluidos(Set<Conteudo> conteudosConcluidos) {
+        this.conteudosConcluidos = conteudosConcluidos;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // TODO Auto-generated method stub
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        // TODO Auto-generated method stub
+        return super.hashCode();
+    }
+
+    
+
+}
