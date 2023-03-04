d10861a87699a5884d4c39d00a92c6330fe7f9b8
tmsfreitas <thiago.msfreitas@hotmail.com>
2023-03-03 22:33:38 -0300

    Adição de classes mentoria e curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setCargaHoraria(8);
+        curso1.setDescricao("Curso de Java");
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Js");
+        curso2.setCargaHoraria(4);
+        curso2.setDescricao("Curso de JavaScript");
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setData(LocalDate.now());
+        mentoria.setDescricao("Descrição Mentoria Java");
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
     }
 }
+    
